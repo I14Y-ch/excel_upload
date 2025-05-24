1d31afe3786fb5ae85e89da410f0728aaa519311
MathiasBornI14Y <mathias.born@bfs.admin.ch>
2025-05-24 22:26:04 +0200

    Adaptations for DO

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 API_BASE_URL = os.environ.get("API_BASE_URL", "https://api.i14y.admin.ch/api/partner/v1")
 
 # Flask web application settings
-FLASK_SECRET_KEY = os.environ.get("FLASK_SECRET_KEY", "development-only-key")  # Only use default for local development
+FLASK_SECRET_KEY = os.environ.get("FLASK_SECRET_KEY", "VnSbK9xUGBwd3ZT8kKiPGNrlO1bJr/7017B+v2/cejo")  # Only use default for local development
 MAX_CONTENT_LENGTH = int(os.environ.get("MAX_CONTENT_LENGTH", 16 * 1024 * 1024))  # 16MB max file size by default
 ALLOWED_EXTENSIONS = {'xlsx'}
 
--- run.py
+++ run.py
@@ -20,4 +20,6 @@ from app.routes import register_routes
 register_routes(app)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    # Use PORT environment variable if available (for Digital Ocean)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
